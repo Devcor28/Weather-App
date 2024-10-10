de908df8c217fecab29e743b354558d1447ca0d0
RubenCornejo <87589878+Devcor28@users.noreply.github.com>
2024-10-10 17:35:21 -0300

    Proyecto final de app del clima con typescript

---
+++ script.js
@@ -0,0 +1,44 @@
+"use strict";
+const api_key = `90ef2e81f55d1bb4e1860d44114e4e2f`;
+const url = `https://api.openweathermap.org/data/2.5/weather`;
+const difKelvin = 273.15;
+const buscar = document.getElementById("buscar");
+const card = document.getElementsByClassName("card")[0];
+const mensajeCiudad = document.getElementsByClassName("mostrarMensaje")[0];
+const ciudadCompletar = document.getElementsByClassName("ciudadCompletar")[0];
+const pais = document.getElementsByClassName("pais")[0];
+const fecha = document.getElementsByClassName("fecha")[0];
+const tiempoTranscurrido = Date.now();
+const hoy = new Date(tiempoTranscurrido);
+const temperatura = document.getElementsByClassName("temperatura")[0];
+const humedad = document.getElementsByClassName("humedad")[0];
+var imagenACompletar;
+const imagen = document.getElementsByClassName("imagen")[0];
+function fetchDatosClima(ciudad) {
+    fetch(`${url}?q=${ciudad}&appid=${api_key}`)
+        .then(response => response.json())
+        .then(response => mostrarDatosClima(response));
+}
+buscar === null || buscar === void 0 ? void 0 : buscar.addEventListener("click", () => {
+    const ciudad = document.getElementById("ciudad").value;
+    fetchDatosClima(ciudad);
+});
+function mostrarDatosClima(response) {
+    const main = response.main;
+    if (main == undefined) {
+        mensajeCiudad.classList.remove("mostrarMensaje");
+    }
+    else {
+        mensajeCiudad.classList.add("mostrarMensaje");
+        card.classList.remove("visible");
+        ciudadCompletar.textContent = response.name;
+        pais.textContent = response.sys.country;
+        fecha.textContent = hoy.toLocaleDateString();
+        let temp = response.main.temp;
+        let tempCelsius = temp - difKelvin;
+        temperatura.textContent = `${Math.floor(tempCelsius)}°C`;
+        humedad.textContent = `${response.main.humidity}%`;
+        let imagenACompletar = response.weather[0].icon;
+        imagen.setAttribute("src", `https://openweathermap.org/img/wn/${imagenACompletar}@2x.png`);
+    }
+}
